688e2b8b20c068229e03bc5c41afe78cbd0035a8
Bernardo Bressan <bressan.mattos@estudante.ufjf.br>
2025-01-16 21:09:02 -0300

    adiciona codigo da aula anterior pois ocorreu um bug de sincronização

--- src/Atributo.tsx
+++ src/Atributo.tsx
@@ -1,34 +1,15 @@
 import "./Atributo.css";
-import { useState } from "react";
 
 type AtributoProps =
 {
     icone: string;
+    valor: number;
 }
-export default function Atributo ({icone} : AtributoProps ){
-
-    const [valor, setValor] = useState(5);
-    
-    function onMenosClick()
-    {
-        if(valor == 0)
-        {
-            setValor(5);
-        }
-        else
-        {
-            setValor(valor - 1);
-        }
-
-    }
-
-
-
+export default function Atributo ({icone, valor} : AtributoProps ){
 
     return <div className="atributo"> 
         <span>{icone.repeat(valor)}</span>
         <span className="inativo">{icone.repeat(5-valor)}</span>
-        <button onClick={onMenosClick}>-</button>
     </div>
 
 }
--- src/Emoji.tsx
+++ src/Emoji.tsx
@@ -14,49 +14,76 @@ const EMOJIS = new Map <string, string> ([
 export default function Emoji (){
 
     //utilização do useState
-    const [estado, setEstado] = useState("clear");
+    const [situacao, setSituacao] = useState("clear");
+    const [saude, setSaude] = useState(4);
+    const [energia, setEnergia] = useState(3);
+    const [comida, setComida] = useState(2);
+    const [agua, setAgua] = useState(1);
+    const [luz, setLuz] = useState(true);
 
-    function toAlive(){
-        setEstado("alive");
+
+
+
+
+    function onAlimentar() {
+        setComida(Math.min(comida+1,5));
     }
-    function toDead(){
-        setEstado("dead");
+    function onHidratar() {
+        setAgua(Math.min(comida+1,5));
     }
-    function toSick(){
-        setEstado("sick");
+    function onLigaDesligaLuz() {
+        setLuz(!luz);
     }
+
 
     //funcao para ciclar correta
-    function cicle() {
-        switch (estado) {
-            case "clear":
-                setEstado("alive");
-                break;
-            case "alive":
-                setEstado("dead");
-                break;
-            case "dead":
-                setEstado("sick");
-                break;
-            case "sick":
-                setEstado("clear");
-                break;
+    function onCiclo() {
+        setComida(Math.max(0,comida - 1));
+        setAgua(Math.max(0,agua - 1));
+        setEnergia(Math.max(0,energia - 1));
+        setSaude(Math.max(0,saude - 1));
+
+        if (luz)
+        {
+            setEnergia(Math.max(0, energia - 1));
+        }
+        else 
+        {
+            setEnergia(Math.min(5, energia - 1));
+        }
+
+        if (comida === 0)
+        {
+            setSaude(s => (Math.max(0,s-1) ));
+        }
+
+        if (agua === 0)
+        {
+            setSaude(s => (Math.max(0,s-1) ));
+        }
+
+        if (energia === 0)
+        {
+            setSaude(s => (Math.max(0,s-1) ));
         }
     }
 
     return <div className="emoji"> 
-        <div className="situacao">{EMOJIS.get(estado) || "🐲"}</div> 
+        <div className="situacao">{EMOJIS.get(situacao) || "🐲"}</div> 
         <div className="atributos">
-            <Atributo icone="❤️"/>
-            <Atributo icone="⚡"/>
-            <Atributo icone="🍗"/>
-            <Atributo icone="🥤"/>
+            <Atributo icone="❤️" valor={saude}/>
+            <Atributo icone="⚡" valor={energia}/>
+            <Atributo icone="🍗" valor={comida}/>
+            <Atributo icone="🥤" valor={agua}/>
+            <span style={{filter: luz?"":"grayscale(100%)"}}>
+                💡
+            </span>     
         </div>
         <div className="acoes">
-            <button onClick={toAlive}> Bicho Vivo </button>
-            <button onClick={toDead}> Bicho Morto </button>
-            <button onClick={toSick}> Bicho Doente </button>
-            <button onClick={cicle}> Próximo </button>
+            <button onClick={onAlimentar}> Dar comida </button>
+            <button onClick={onHidratar}> Dar água </button>
+            <button onClick={onLigaDesligaLuz}> {luz ? "Apagar" : "Acender"} a luz </button>
+            <button onClick={onCiclo}> Ciclo </button>
         </div>
     </div>
     
